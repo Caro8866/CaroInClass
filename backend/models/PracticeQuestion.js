d0c0abc072858c4e8949e72a00bd97a2fc637f36
Caroline Thostrup <caro342h@stud.kea.dk>
2023-10-11 19:30:58 +0200

    rename

--- backend/models/PracticeQuestion.js
+++ backend/models/PracticeQuestion.js
@@ -1,10 +1,10 @@
 import mongoose from "mongoose";
 
-const PracticeQuestionsSchema = new mongoose.Schema({
+const PracticeQuestionSchema = new mongoose.Schema({
   question: { type: String, required: true, unique: true },
   answer: { type: String, required: true },
   type: { type: String, required: true }, // multiple choice, true/false, fill in the blank, code, etc...
   classID: { type: mongoose.Schema.Types.ObjectId, ref: "Class", required: true },
 });
 
-module.exports = mongoose.model("PracticeQuestions", PracticeQuestionsSchema);
+module.exports = mongoose.model("PracticeQuestion", PracticeQuestionSchema);
