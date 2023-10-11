a783d5cf309a8948c0177bb734c9159ca03dcd2b
Caroline Thostrup <caro342h@stud.kea.dk>
2023-10-11 19:16:53 +0200

    add class schema

---
+++ backend/models/ClassSchema.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+// teacherschema embedded document
+const teacherSchema = mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+});
+
+const ClassSchema = mongoose.Schema({
+  name: { type: String, required: true, unique: true },
+  teacher: teacher, // embedded document
+  semester: { type: String, required: true },
+  year: { type: Number, required: true },
+  // students: { type: Array, required: true }, // not relevant right now
+});
+
+const Class = mongoose.model("Class", ClassSchema);
